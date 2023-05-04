e780cf435058d1ceff0b1bf8c4d9dd792b9f9995
oxpas <oxpase@gmail.com>
2023-05-03 22:13:33 -0400

    added SquadCopy

---
+++ src/main/java/com/example/ChatListener.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.event.ClickEvent;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class ChatListener {
+    @SubscribeEvent
+    public void onChatReceived(ClientChatReceivedEvent event) {
+        String playerName = Minecraft.getMinecraft().thePlayer.getName();
+        String message = event.message.getUnformattedText();
+
+        if (message.contains(", " + playerName + ",")) {
+            String modifiedMessage = message.replace(",", "").replace(".", "").replace("§7", "");
+            String finalMessage = EnumChatFormatting.BLUE + "squad add " + EnumChatFormatting.GREEN + modifiedMessage +EnumChatFormatting.YELLOW + " [CopySquadForCuteOvO]";
+            String unformattedFinalMessage = "/squad add " + modifiedMessage;
+
+            ChatComponentText chatComponent = new ChatComponentText(finalMessage);
+
+            // Set up the clickable chat style
+            ChatStyle clickableStyle = new ChatStyle();
+            clickableStyle.setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, unformattedFinalMessage));
+            chatComponent.setChatStyle(clickableStyle);
+
+            Minecraft.getMinecraft().thePlayer.addChatMessage(chatComponent);
+        }
+    }
+}
--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -1,13 +1,17 @@
 package com.example;
 
 import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", version = "1.0.0")
+@Mod(modid = "OkwCopySquad", version = "1.0.3")
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
+
+        // Register the chat listener
+        MinecraftForge.EVENT_BUS.register(new ChatListener());
     }
 }
