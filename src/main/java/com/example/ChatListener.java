4030d73e2d046c76e48d5840f5e4b86f60b2b752
oxpas <oxpase@gmail.com>
2023-06-01 18:40:05 -0400

    message.matches(".*RED: \\d+ -.*") 现在判断"RED: <digits> - <anything>" "BLUE: <digits> - <anything>" 以此类推

--- src/main/java/com/example/ChatListener.java
+++ src/main/java/com/example/ChatListener.java
@@ -9,12 +9,20 @@ import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ChatListener {
+
     @SubscribeEvent
     public void onChatReceived(ClientChatReceivedEvent event) {
         String playerName = Minecraft.getMinecraft().thePlayer.getName();
         String message = event.message.getUnformattedText();
 
-        if (message.contains(", " + playerName + ".")|| message.contains("- " + playerName + ",")||message.contains(", " + playerName) ) {
+        if (message.contains(", " + playerName + ".")
+                || message.contains("- " + playerName + ",")
+                || message.contains(", " + playerName)
+                || message.matches(".*YELLOW: \\d+ -.*")
+                || message.matches(".*GREEN: \\d+ -.*")
+                || message.matches(".*RED: \\d+ -.*")
+                ||message.matches(".*BLUE: \\d+ -.*")) {
+        //if (message.contains(", " + playerName + ".")|| message.contains("- " + playerName + ",")||message.contains(", " + playerName) ) {
             String modifiedMessage = message.replace(",", "").replace(".", "").replace("§7", "");
             String finalMessage = EnumChatFormatting.BLUE + "squad add " + EnumChatFormatting.GREEN + modifiedMessage +EnumChatFormatting.YELLOW + " [CopySquadOvO]";
             String unformattedFinalMessage = "/squad add " + modifiedMessage;
